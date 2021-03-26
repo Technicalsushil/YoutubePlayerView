6f3f14575f6b0a3a0423434fa3e9bd29ea7923c5
Oad786 <36334253+Oad786@users.noreply.github.com>
2021-03-26 17:59:16 +0530

    YoutubePlayerView

---
+++ app/src/main/java/com/example/youtubevideo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.youtubevideo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        YouTubePlayerView ytview=findViewById(R.id.ytview);
+        getLifecycle().addObserver(ytview);
+
+        ytview.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "srflia08x5o";
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
+    }
+}
